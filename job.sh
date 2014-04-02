e1eb85b98e6a0ea2c0a7d7ab79c39526544f679a
vikingfenn <fjosee@rpi.edu>
2014-04-02 15:52:19 -0400

    now does all node/thread configurations

--- job.sh
+++ job.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-
+rm results.txt
+for j in 64 16 4 1
+do
+  echo "$j tasks with $((64/$j)) threads on 64 nodes"
+  srun --partition=small --time=15 --overcommit --runjob-opts="--mapping TEDCBA" --nodes=64 --ntasks-per-node=$j ./a.out $((64/$j)) >> results.txt
+  if [ $? -ne 0 ]
+    then exit 0
+  fi
+  echo "DONE!"
+done
 for i in 128 256
+do
+  for j in 64 16 4 1
   do
-  for j in 1 2 4
-    do
-    srun --partition=medium --time=10 --overcommit --runjob-opts="--mapping TEDCBA" --nodes=$i --ntasks=$(($i*$j)) ./MPI3
-    echo $i $j
+    echo "$j tasks with $((64/$j)) threads on $i nodes"
+    srun --partition=medium --time=15 --overcommit --runjob-opts="--mapping TEDCBA" --nodes=$i --ntasks-per-node=$j ./a.out $((64/$j)) >>results.txt
+    if [ $? -ne 0 ]
+      then exit 0
+    fi
+    echo "DONE!"
   done
 done
