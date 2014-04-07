a7885b365f34423febd1ba6bb09f446f14f5480c
Nicholas Pitt <ngpitt@iecfusor.com>
2014-04-07 14:09:32 -0400

    Updated Job Script
    Added new command line arguments to the job script for file output.

--- job.sh
+++ job.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-rm results.txt
-for j in 64 16 4 1
-do
-  echo "$j tasks with $((64/$j)) threads on 64 nodes"
-  srun --partition=small --time=15 --overcommit --runjob-opts="--mapping TEDCBA" --nodes=64 --ntasks-per-node=$j ./a.out $((64/$j)) >> results.txt
-  if [ $? -ne 0 ]
-    then exit 0
-  fi
-  echo "DONE!"
-done
-for i in 128 256
-do
-  for j in 64 16 4 1
-  do
-    echo "$j tasks with $((64/$j)) threads on $i nodes"
-    srun --partition=medium --time=15 --overcommit --runjob-opts="--mapping TEDCBA" --nodes=$i --ntasks-per-node=$j ./a.out $((64/$j)) >>results.txt
-    if [ $? -ne 0 ]
-      then exit 0
-    fi
-    echo "DONE!"
-  done
+> results.txt
+for NODES in 64 128 256; do
+    for TASKS in 1 4 16 64; do
+        for RANKS_PER_FILE in 1 4 8 32; do
+            for COMPACT in 0 1; do
+                THREADS=$((64/$TASKS))
+                PARTITION="small"
+                if [ $NODES -gt 64 ]; then
+                    PARTITION="medium"
+                fi
+                echo "Running $TASKS tasks with $THREADS threads on $NODES nodes"
+                srun --partition=$PARTITION --time=15 --overcommit --runjob-opts="--mapping TEDCBA" --nodes=$NODES --ntasks-per-node=$TASKS ./a.out $THREADS $RANKS_PER_FILE $COMPACT >> results.txt
+                if [ $? -ne 0 ]
+                then
+                    echo "Error: Non-zero return code"
+                    exit 0
+                fi
+                echo "Success"
+            done
+        done
+    done
 done
